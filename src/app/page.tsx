0970b20d3932b77c3534fde6537e398946c1df84
Nur Syazmiza <syazmiza0304@gmail.com>
2026-02-19 01:53:13 +0000

    when i click the login button on the get started page , make sure it dir

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -60,7 +60,7 @@ export default function Home() {
                 </p>
                 <div className="flex flex-col gap-2 min-[400px]:flex-row">
                   <Button size="lg" asChild>
-                    <Link href={user ? "/dashboard" : "/login"}>Get Started</Link>
+                    <Link href="/login">Get Started</Link>
                   </Button>
                 </div>
               </div>
