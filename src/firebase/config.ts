b88764d64e8f876f4e9ead4e3ce63c82d86c535f
Nur Syazmiza <syazmiza0304@gmail.com>
2026-04-18 10:32:01 +0000

    refer to this for the storage bucket ,include storageBucket property: st

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,6 +1,8 @@
+
 export const firebaseConfig = {
   "projectId": "studio-1474016013-65266",
   "appId": "1:3639989136:web:fa0fa6c5cdd5ed88d39915",
+  "storageBucket": "studio-1474016013-65266.firebasestorage.app",
   "apiKey": "AIzaSyB2Clrx8lr_yV3h7-PSXuAnUxtS0HaW74c",
   "authDomain": "studio-1474016013-65266.firebaseapp.com",
   "measurementId": "",
