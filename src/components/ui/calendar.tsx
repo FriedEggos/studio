3bb9e281c888a50ad99d1492dadf402d34f8d8c8
Nur Syazmiza <syazmiza0304@gmail.com>
2026-04-26 04:15:05 +0000

    pleaase resolve this issue so i can deploy my webstie smoothly:
    (error

--- src/components/ui/calendar.tsx
+++ src/components/ui/calendar.tsx
@@ -41,10 +41,10 @@ function Calendar({
         nav_button_previous: "absolute left-1",
         nav_button_next: "absolute right-1",
         table: "w-full border-collapse space-y-1",
-        head_row: "grid grid-cols-7",
+        head_row: "flex",
         head_cell:
-          "text-muted-foreground rounded-md font-normal text-[0.8rem] text-center",
-        row: "grid grid-cols-7 w-full mt-2",
+          "text-muted-foreground rounded-md w-9 font-normal text-[0.8rem]",
+        row: "flex w-full mt-2",
         cell: "h-9 w-9 text-center text-sm p-0 relative [&:has([aria-selected].day-range-end)]:rounded-r-md [&:has([aria-selected].day-outside)]:bg-accent/50 [&:has([aria-selected])]:bg-accent first:[&:has([aria-selected])]:rounded-l-md last:[&:has([aria-selected])]:rounded-r-md focus-within:relative focus-within:z-20",
         day: cn(
           buttonVariants({ variant: "ghost" }),
@@ -64,7 +64,7 @@ function Calendar({
       }}
       components={{
         Dropdown: ({ value, onChange, options }: DropdownProps) => {
-          const selected = options?.find((option) => option.value === value)
+          const selected = options.find((option) => option.value === value)
           const handleChange = (value: string) => {
             const changeEvent = {
               target: { value },
@@ -83,7 +83,7 @@ function Calendar({
               </SelectTrigger>
               <SelectContent position="popper">
                 <ScrollArea className="h-80">
-                  {options?.map((option, id: number) => (
+                  {options.map((option, id: number) => (
                     <SelectItem
                       key={`${option.value}-${id}`}
                       value={option.value.toString()}
@@ -96,8 +96,8 @@ function Calendar({
             </Select>
           )
         },
-        IconLeft: ({ ...props }) => <ChevronLeft className="h-4 w-4" />,
-        IconRight: ({ ...props }) => <ChevronRight className="h-4 w-4" />,
+        IconLeft: () => <ChevronLeft className="h-4 w-4" />,
+        IconRight: () => <ChevronRight className="h-4 w-4" />,
       }}
       {...props}
     />
